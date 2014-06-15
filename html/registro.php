f3769ee38859294dcfd16bcc488f9ae17e1fbde5
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-06-14 23:58:01 -0500

    Primer commit

---
+++ html/registro.php
@@ -0,0 +1,76 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registro ACFriendCodes</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Bootstrap -->
+        <link href="/bootstrap-3.1.1-dist/css/bootstrap.css" rel="stylesheet" media="screen">
+        <script src="/bootstrap-3.1.1-dist/assets/js/jquery.js"></script>
+        <script src="/bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
+        <!-- Estilos -->
+        <link href="/css/estilos.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <?php
+        $html_nav = file_get_contents("layouts/navbar_nologin_layout.html");
+        $doc = new DOMDocument();
+        $doc->loadHTML(mb_convert_encoding($html_nav, 'HTML-ENTITIES', 'UTF-8'));
+        ?>
+
+        <div id="nav_bar"><?php echo $doc->saveHTML(); ?></div>
+        <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">REGISTRO</h3>
+                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label for="usr_nickname" class="col-sm-2 control-label">Nickname: </label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="usr_nickname" placeholder="Nickname">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="usr_email" class="col-sm-2 control-label">Email: </label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="usr_nickname" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="usr_password" class="col-sm-2 control-label">Password: </label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="usr_password" placeholder="Password">
+                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="usr_password_conf" class="col-sm-2 control-label">Confirmacion Password: </label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="usr_password_conf" placeholder="Confirma tu password">
+                            </div>
+                        </div>
+                       
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-success">Registrarse!</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Funciona</h1>
+        <a href="html/registro.php">Registro</a>
+    </body>
+</html>
